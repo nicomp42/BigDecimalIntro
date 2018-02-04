3f01af34840315305eafef3e2184eea20097d58a
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-02-04 09:45:49 -0500

    More examples

--- BigDecimalIntro/src/main/Main.java
+++ BigDecimalIntro/src/main/Main.java
@@ -3,36 +3,105 @@ package main;
 import java.math.BigDecimal;
 
 /**
- * Demonstration of BigDecimal class	
+ * Demonstration of BigDecimal class
  * @author nicomp
  *
  */
 public class Main {
 
-	
+
 	public static void main(String[] args) {
 		demo();
 	}
 	/**
-	 * Demonstrate the 
+	 * Demonstrate the
 	 */
 	public static void demo() {
 
 		BigDecimal bigDecimalAmount = new BigDecimal("14.00");
 		double doubleAmount = 14.00;
-		
+
 		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
-		System.out.println("Double Amount = " + doubleAmount);
+		System.out.println("Double Amount      = " + doubleAmount);
+		System.out.println("---------------------------------------------------------");
 
 		// Apply 7.5% sales tax to both amounts
 		bigDecimalAmount = bigDecimalAmount.multiply(new BigDecimal("1.075"));
 		doubleAmount *= 1.075;
 
+		// Some error creeps in here when we use double
 		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
-		System.out.println("Double Amount = " + doubleAmount);
+		System.out.println("Double Amount      = " + doubleAmount);
+		System.out.println("---------------------------------------------------------");
+
+		// We can make the error much worse
+		bigDecimalAmount = new BigDecimal("1234567899");
+		doubleAmount = 1234567899;
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.printf("Double Amount      = %.2f \n", doubleAmount);
+		System.out.println("---------------------------------------------------------");
+
+		// Add one penny. No problem.
+		bigDecimalAmount = bigDecimalAmount.add(new BigDecimal(".01"));
+		doubleAmount += .01;
+		System.out.println("---------------------------------------------------------");
+
+		// Print again
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.printf("Double Amount      = %.2f \n" , doubleAmount);
+		System.out.println("---------------------------------------------------------");
+/*
+		// Add a billion microtransactions of .0001 to both variables. That would be 1,000,000,000 * .0001 = $100,000 dollars. Big Problem.
+
+		for (int i = 0; i < 1_000_000_000; i++) {	// This will take a while
+			bigDecimalAmount = bigDecimalAmount.add(new BigDecimal(".0001"));
+			doubleAmount += .0001;
+		}
+		// Print again
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.printf("Double Amount      = %.2f \n" , doubleAmount);
+		System.out.println("---------------------------------------------------------");
+*/
+
+/*
+		// Reset to zero and add a billion microtransactions of .000715 (sales tax on one penny) to both variables. That would be 1,000,000,000 * .00075 = $750,000 dollars. Big Problem.
+		bigDecimalAmount = new BigDecimal("0");
+		doubleAmount = 0;
+
+		for (int i = 0; i < 1_000_000_000; i++) {	// This will take a while
+			bigDecimalAmount = bigDecimalAmount.add(new BigDecimal(".000715"));
+			doubleAmount += .000715;
+		}
+		// Print again
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.printf("Double Amount      = %.2f \n" , doubleAmount);
+		System.out.println("---------------------------------------------------------");
+*/
+		// One microtransaction of one tenth of a penny and sales tax on that
+		bigDecimalAmount = new BigDecimal(".0010715");
+		doubleAmount = .0010715;
+		// Print again
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.println("Double Amount      = " + doubleAmount);		
+		System.out.println("---------------------------------------------------------");
+		
+/*		
+		// Reset to zero and add a billion microtransactions of .0010715 (one tenth of a penny and sales tax on that) to both variables. That would be 1,000,000,000 * .001075 = $1,750,000 dollars. Big Problem.
+		bigDecimalAmount = new BigDecimal("0");
+		doubleAmount = 0;
+
+		for (int i = 0; i < 1_000_000_000; i++) {	// This will take a while
+			bigDecimalAmount = bigDecimalAmount.add(new BigDecimal(".0010715"));
+			doubleAmount += .0010715;
+		}
+		// Print again
+		System.out.println("Big Decimal Amount = " + bigDecimalAmount.toString());
+		System.out.printf("Double Amount      = %.2f \n" , doubleAmount);
+		System.out.println("---------------------------------------------------------");
+*/
 		
 		
 		
 	}
-	
+
 }
